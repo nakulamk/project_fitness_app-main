60188fa5c38b28bda7e6002b838e10f3f1fffa25
Nakula Gowda <nakulamk@gmail.com>
2023-07-20 17:58:29 +0530

    API-updataion

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -10,7 +10,7 @@ export const youtubeOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "dfe3ce08eamsh9551c900a1b755ep1dc34cjsna7155636dc66",
   },
 };
 
